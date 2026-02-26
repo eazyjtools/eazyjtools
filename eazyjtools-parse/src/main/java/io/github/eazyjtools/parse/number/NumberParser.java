0c44e3b89fca1b9fe5cd20af48ce580ba5e7262d
romanzdev <romanzolotdev@gmail.com>
2026-02-26 14:46:37 +0200

    add new NumberParser uility class

---
+++ eazyjtools-parse/src/main/java/io/github/eazyjtools/parse/number/NumberParser.java
@@ -0,0 +1,10 @@
+package io.github.eazyjtools.parse.number;
+
+public final class NumberParser {
+
+	private NumberParser() {
+		throw new UnsupportedOperationException("Utility class cannot be instantiated!");
+	}
+	
+	
+}
